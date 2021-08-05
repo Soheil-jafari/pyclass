b2b42f5768b0e911b1ae20b147c7b5255800fd17
Soheil-jafari <88477503+Soheil-jafari@users.noreply.github.com>
2021-08-05 12:47:20 +0430

    Update s2_3.py

--- s2_3.py
+++ s2_3.py
@@ -2,8 +2,8 @@ name=input("enter a name:")
 age=int(input("enter a number:"))
 
 if name=="ali" and age>30:
-   print("you can enter")
+       print("you can enter")
     elif name!="ali" and age<20:
-      print("you can enter")
+        print("you can enter")
       else:
-        print("you can not")
+         print("you can not")
