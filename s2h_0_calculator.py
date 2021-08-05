c6261eb1ba09baf3767b5c9e6e135a7d6a723a00
Soheil-jafari <88477503+Soheil-jafari@users.noreply.github.com>
2021-08-05 11:21:49 +0430

    Update s2h_0_calculator.py

--- s2h_0_calculator.py
+++ s2h_0_calculator.py
@@ -1,1 +1,13 @@
-dcs
+  
+a=int(input('Addad 1'))
+b=int(input('Addad 2'))
+c=input()
+
+if c=="+" :
+    print('a+b')
+elif c=="*" : 
+    print(a*b)
+elif c=="-" :
+    print(a-b)
+ else :
+    print("balda No")
