a549f1630f5ab6e785f414f93ce2b73b6c969915
Soheil-jafari <88477503+Soheil-jafari@users.noreply.github.com>
2021-08-05 12:46:36 +0430

    Create s2_3.py

---
+++ s2_3.py
@@ -0,0 +1,9 @@
+name=input("enter a name:")
+age=int(input("enter a number:"))
+
+if name=="ali" and age>30:
+   print("you can enter")
+    elif name!="ali" and age<20:
+      print("you can enter")
+      else:
+        print("you can not")
