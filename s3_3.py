c4c43f1e6d14ab7e3b53133d99e0a1eec5eee8fe
Soheil-jafari <88477503+Soheil-jafari@users.noreply.github.com>
2021-08-12 10:21:05 +0430

    Create s3_3.py

---
+++ s3_3.py
@@ -0,0 +1,10 @@
+def func(a, b, darkhast)
+    if darkhast=="mohit":
+       m=2*a+2*b
+       print("mohite mostatil ba toole {} va arze {} mishavad {}".format(a, b, m))
+    elif darkhast=="masahat"
+       s=a*b
+       print("masahat=",s)
+       
+       
+ func()
