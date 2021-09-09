886db5fe523c1c56a81f2d0c9f2c9833d00fe0fd
Soheil-jafari <88477503+Soheil-jafari@users.noreply.github.com>
2021-09-09 09:23:58 +0430

    Create s6_1.py

---
+++ s6_1.py
@@ -0,0 +1,12 @@
+def zarb(a, b)
+   a=input("enter a number")
+   b=input("enter a number")
+   count=0
+   while count!=a:
+       b+=b
+       count+=1
+       
+    return c
+    
+    
+print(c)
